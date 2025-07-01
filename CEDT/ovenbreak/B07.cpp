99ea0e77df7fdad45b414ee6c61bdc59b4b2ec1e
naxocist <subforwork@gmail.com>
2025-07-01 19:34:14 +0700

    Update

--- CEDT/ovenbreak/B07.cpp
+++ CEDT/ovenbreak/B07.cpp
@@ -5,35 +5,24 @@ using namespace std;
 #define ll long long
 #define all(x) begin(x), end(x)
 #define pb emplace_back
-#define int ll
 
-int under(int a, int b) {
-  int n; cin >> n;
-  int x = 1;
-  while(x*x < n) x ++;
-
-  if(x == 1) cout << 0;
-  else if(x <= 2) cout << x-1 + (n != 3);
-  else cout << 2*x-3 + (n-x+1)%2; 
-}
 int32_t main() {
   cin.tie(nullptr)->sync_with_stdio(0);
   int a, b; cin >> a >> b;
   int x = ceil(sqrt(a)), y = ceil(sqrt(b));
-
-  int lx = a - (x-1)*(x-1) - 1, rx = x*x - a;
-  int ly = b - (y-1)*(y-1) - 1, ry = y*y - b;
+  int ar = x*x-a, br = y*y-b;
+  int al = x-ar-1, bl = y-br-1;
 
   if(x == y) cout << a-b, exit(0);
 
-  int gap = max(0LL, x-y-1);
-  if(lx >= ly-(ly%2) and rx >= ry-(ry%2)) {
-    int v = 2*gap + 1 + (rx%2 == 0) + (ry%2);
+  int v = max(0,x-y-1)*2 + 1 + (ar%2==0);
+  if(al >= bl-(bl%2) and ar >= br-(br%2)) {
+    v += br%2; 
     cout << v;
     return 0;
   }
 
-  int v = 2*gap + 1 + (rx%2 == 0);
-  cout << v + min(abs(lx-(lx%2)-ly), abs(rx-(rx%2)-ry));
+  int h = min(abs(al-(al%2)-bl), abs(ar-(ar%2)-br));
+  cout << v + h;
   return 0;
 }
