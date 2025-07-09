9bd974c90990ec6731f1d102bb57cef145b75192
naxocist <subforwork@gmail.com>
2025-07-09 22:57:24 +0700

    Update

--- CEDT/ovenbreak/D18.cpp
+++ CEDT/ovenbreak/D18.cpp
@@ -1,22 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ln '\n'
 #define ll long long
 #define all(x) begin(x), end(x)
 #define pb emplace_back
 
 const int N = 3e3 + 1;
-int dp[N][N];
+int ln[N], sd[N], hp[N];
 
 int32_t main() {
   cin.tie(nullptr)->sync_with_stdio(0);
   int l, n; cin >> l >> n;
 
-  vector<pi> v(n);
   for(int i=0; i<n; ++i) {
     int x, c, v; cin >> x >> c >> v;
+    ln[i] = x;
+    if(c == 1) sd[i] = v;
+    else hp[i] = v;
   }
+
+  int res = 0;
+  for(int i=0; i<1<<n; ++i) {
+    int a = 0, b = 0, c = 0;
+    for(int j=0; j<n; ++j) {
+      if(i & (1<<j)) {
+        a += ln[j];
+        b += sd[j];
+        c += hp[j];
+      }
+    }
+
+    if(a > l) continue ;
+    res = max(min(b, c), res);
+  }
+
+  cout << res;
 
   return 0;
 }
---
+++ CEDT/ovenbreak/D18_dp.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 5e3 + 3;
+int l[N], s[N], h[N];
+
+
+struct state {
+  int sad = -1, happy = -1;
+};
+
+int main() {
+  int lim, n; cin >> lim >> n;
+  vector<state> dp(lim + 1);
+  dp[0].sad = 0, dp[0].happy = 0;
+
+  for(int i=0; i<n; ++i) {
+    int x, c, v; cin >> x >> c >> v;
+    l[i] = x;
+    if(c == 1) s[i] = v;
+    else h[i] = v;
+
+    for(int j=lim; j>=0; --j) {
+      int nl = j + l[i];
+      int ns = dp[j].sad + s[i];
+      int nh = dp[j].happy + h[i];
+      if(j + l[i] > lim) continue ;
+      if(dp[nl].sad == -1 or min(dp[nl].sad,dp[nl].happy) < min(ns, nh)) {
+        dp[nl].sad = ns, dp[nl].happy = nh;
+      }
+    }
+  }
+
+
+  int res = 0;
+  for(int i=0; i<=lim; ++i) {
+    res = max(res, min(dp[i].sad, dp[i].happy));
+  }
+  cout << res;
+  return 0;
+}
