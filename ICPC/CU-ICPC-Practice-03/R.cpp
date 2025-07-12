b068fb3b0d2b29edb2e16730f4b794422315bfb7
naxocist <subforwork@gmail.com>
2025-07-12 16:58:41 +0700

    Update

--- ICPC/CU-ICPC-Practice-03/R.cpp
+++ ICPC/CU-ICPC-Practice-03/R.cpp
@@ -7,19 +7,40 @@ using namespace std;
 #define pb emplace_back
 
 const int N = 2e5 + 1;
-vector<int> adj[N];
+set<int> adj[N];
 
 void solve() {
-  int n; cin >> n;
+  ll n; cin >> n;
 
-  for(int i=1; i<=n; +++i) adj[i].clear();
+  for(int i=1; i<=n; ++i) adj[i].clear();
 
   for(int i=0; i<n; ++i) {
     int u, v; cin >> u >> v;
-    adj[u].pb(v), adj[v].pb(u);
+    adj[u].insert(v), adj[v].insert(u);
+  }
+
+  queue<int> q;
+  for(int i=1; i<=n; ++i) {
+    if(adj[i].size() == 1) q.emplace(i);
   }
 
 
+  vector<ll> v(n + 1, 1);
+  while(q.size()) {
+    int u = q.front(); q.pop();
+    int p = *(adj[u].begin());
+    v[p] += v[u];
+    v[u]=0;
+    adj[p].erase(u);
+    if(adj[p].size() == 1) q.emplace(p);
+  }
+
+  ll res = 0;
+  for(int i=1; i<=n; ++i) {
+    res += v[i]*(v[i]-1)/2;
+    res += v[i]*(n-v[i]);
+  }
+  cout << res << ln;
 }
 
 int32_t main() {
