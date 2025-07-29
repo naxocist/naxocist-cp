9c6b82eff738f5dd76a20170a82a1b3dfc74a672
naxocist <subforwork@gmail.com>
2025-07-29 11:32:59 +0700

    Update

--- what.cpp
+++ what.cpp
@@ -1,39 +1,34 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-void dbg_out() { cerr << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
-#ifdef LOCAL
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 
-#define ar array
-#define ll long long
-#define ld long double
-#define sza(x) ((int)x.size())
-#define all(a) (a).begin(), (a).end()
+template <typename T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+using ll = long long;
+using ld = long double;
+
 #define pb emplace_back
-
-const ld EPS = 1e-9;
-const int N = 2e5 + 3;
-
-void solve() {
-  int n; cin >> n;
-
-}
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
 
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0); cout.tie(0);
-  int tc = 1;
-  // cin >> tc;
-  for (int t=1; t<=tc; ++t) {
-    // cout << "Case #" << t << ": ";
-    solve();
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  int n; cin >> n;
+  vector<int> v(n);
+
+  for(int i=1; i<=n; ++i) {
+    int x; cin >> x;
+    v[i] = v[i-1] + x;
   }
+
+  int n; cin >> n; cout << n * n << '\n';
+  int x; cin >> x; cout << x * x << '\n';
+
+  cout << v[n] << '\n';
+
+  return 0;
 }
