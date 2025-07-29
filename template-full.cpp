8a188147a3c213fce316dde8abad1173468bb616
naxocist <subforwork@gmail.com>
2025-07-29 11:34:08 +0700

    Update

--- template-full.cpp
+++ template-full.cpp
@@ -3,18 +3,10 @@ using namespace std;
 
 #define ln '\n'
 #define ll long long
-#define pii pair<int, int>
-#define pil pair<int, ll>
-#define pli pair<ll, int>
-#define pll pair<ll, ll>
+#define ld long double
+
 #define all(x) x.begin(), x.end()
 #define pb emplace_back
-#define p(x) cout << #x << " = " << x << '\n';
-#define p2(x) cout << #x << " = {" << x.first << ',' << x.second << "}\n";
-#define pv(a) cout << #a << " = "; for(auto _x : a) cout << _x << ' '; cout << '\n';
-#define pv2(a) cout << #a << " = "; for(auto [_x,_y] : a)cout << '{' << _x << ',' << _y << "} "; cout << '\n';
-#define pa1(a,n) cout << #a << " = "; for(int _i = 1; _i <= n; ++_i)cout << a[_i] << ' '; cout << '\n';
-#define pa0(a,n) cout << #a << " = "; for(int _i = 0; _i < n; ++_i)cout << a[_i] << ' '; cout << '\n';
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int uid(int a, int b) { return uniform_int_distribution<int>(a,b)(rng);}
